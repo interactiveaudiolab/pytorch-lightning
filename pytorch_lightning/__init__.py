763a75c03d8fa7bdb8a07ff2d2f90da5d2f60392
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-09-15 23:49:50 +0200

    fix?

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -35,8 +35,8 @@ Documentation
 
 import logging as python_logging
 
-_logger = python_logging.getLogger("lightning")
-# _logger.addHandler(python_logging.StreamHandler())
+_logger = python_logging.getLogger(__name__)
+_logger.addHandler(python_logging.StreamHandler())
 _logger.setLevel(python_logging.INFO)
 
 try:
