79830f21d0b0334a5ab9750a2d3c741bc34eea22
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-09-20 08:22:57 +0200

    det

---
+++ pl_examples/basic_examples/eval.py
@@ -0,0 +1,41 @@
+from pytorch_lightning import seed_everything, Trainer
+from tests.base import EvalModelTemplate
+from tests.base.datamodules import TrialMNISTDataModule
+
+import torch
+import numpy
+
+
+def foo():
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+    seed_everything(1234)
+
+    print(torch.rand(1,2))
+    print(numpy.random.uniform(0, 1, 3))
+
+    dm = TrialMNISTDataModule(".")
+
+    model = EvalModelTemplate()
+
+    trainer = Trainer(
+        default_root_dir=".",
+        max_epochs=5,
+        weights_summary=None,
+        distributed_backend='ddp_spawn',
+        gpus=[0, 1],
+        deterministic=True,
+    )
+
+    # fit model
+    result = trainer.fit(model, dm)
+    assert result == 1
+
+    # test
+    result = trainer.test(datamodule=dm)
+    result = result[0]
+    assert result['test_acc'] > 0.8
+
+
+if __name__ == "__main__":
+    foo()
